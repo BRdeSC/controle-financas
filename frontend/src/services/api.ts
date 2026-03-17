ce208781302ae1a899330d330cf4514e689539b1
BRdeSC <brunodsc.trabalho@gmail.com>
2026-03-17 16:28:01 -0300

    fix: ajuste no CMD do docker para rodar db push e manter servidor vivo

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  //baseURL: 'http://localhost:3333',
+  baseURL: "https://api-financas-backend-kr7n.onrender.com"
 });
 
 // Esse trecho abaixo é "mágico": ele tenta pegar o token do navegador 
